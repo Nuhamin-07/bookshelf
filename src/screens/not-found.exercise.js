571d8abce5be46e1a76f270e3356ceab34fb2cc8
Nuhamin Gulilat <nuhamin.gulilat.7@gmail.com>
2022-05-27 13:22:41 +0300

    route exercise

--- src/screens/not-found.exercise.js
+++ src/screens/not-found.exercise.js
@@ -1,9 +1,9 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core'
+import { jsx } from '@emotion/core'
 
 // This page displays a regular link on the page, and we've got a styled component
 // for that.
-// 🐨 Import the Link from 'components/lib'
+import { Link } from 'components/lib'
 // 💰 and feel free to take a peek at the implementation of that styled component
 // to be reminded how you can create styled components out of existing components
 
@@ -19,10 +19,11 @@ function NotFoundScreen() {
     >
       <div>
         Sorry... nothing here.
+        <Link to="/discover">Go Home</Link>
         {/* 🐨 add a <Link> here that says "Go home" and sends the user to "/discover" */}
       </div>
     </div>
   )
 }
 
-export {NotFoundScreen}
+export { NotFoundScreen }
